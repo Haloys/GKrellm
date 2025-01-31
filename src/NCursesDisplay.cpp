f00f988272b33942e97a558e9060a920e7517d98
aloys <aloys.mangin@epitech.eu>
2025-01-31 23:30:50 +0100

    feat: Add drawModule function to NCursesDisplay for displaying module information

--- src/NCursesDisplay.cpp
+++ src/NCursesDisplay.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "NCursesDisplay.hpp"
+#include "IModule.hpp"
 
 Krell::NCursesDisplay::NCursesDisplay() : _isRunning(false)
 {
@@ -24,6 +25,8 @@ void Krell::NCursesDisplay::start()
     _window = initscr();
     noecho();
     cbreak();
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
     keypad(_window, TRUE);
     _isRunning = true;
 }
@@ -50,4 +53,24 @@ void Krell::NCursesDisplay::handleEvents()
 bool Krell::NCursesDisplay::isRunning() const
 {
     return _isRunning;
+}
+
+void Krell::NCursesDisplay::drawModule(const IModule& module)
+{
+    static int yPos = 1;
+
+    mvprintw(yPos, 1, "CPU Usage");
+    float usagePercent = static_cast<float>(module.getValue("used")) /
+                        static_cast<float>(module.getValue("total")) * 100.0f;
+    mvprintw(yPos + 1, 1, "[");
+    int barWidth = 50;
+    int usedWidth = static_cast<int>((usagePercent / 100.0f) * barWidth);
+    attron(COLOR_PAIR(1));
+    for (int i = 0; i < usedWidth; i++)
+        mvprintw(yPos + 1, 2 + i, "|");
+    attroff(COLOR_PAIR(1));
+    for (int i = usedWidth; i < barWidth; i++)
+        mvprintw(yPos + 1, 2 + i, " ");
+    mvprintw(yPos + 1, 2 + barWidth, "] %d%%", static_cast<int>(usagePercent));
+    yPos += 3;
 }
